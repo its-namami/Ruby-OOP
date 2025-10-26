b46f7bb1d759377f6b1a0f6be05be691261ef038
its-namami <164639260+its-namami@users.noreply.github.com>
2025-10-26 13:28:13 +0100

    refactor: make test method smaller, add frozen str literal

--- main.rb
+++ main.rb
@@ -1,15 +1,12 @@
+# frozen_string_literal: true
+
 require_relative 'lib/my_car'
 
 def test_my_car
-  car = MyCar.new model: 'Toyota A35', color: 'White', year: '1987'
-  car.speed_up
-  car.speed_up
-  car.brake
-  car.speed_up
+  car = MyCar.new model: 'Toyota A36', color: 'White', year: '1987'
+  3.times { car.speed_up }
   car.shut_off
-  car.brake
-  car.brake
-  car.brake
+  3.times { car.brake }
   car.shut_off
   car.pp_color
   car.spray_paint 'blue'
